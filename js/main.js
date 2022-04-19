585edc9bac46eede5d241d3d4de057675807b960
hecris10 <helamaewerton@gmail.com>
2022-04-19 19:33:07 -0300

    Add main-index

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const links = [
+  {
+    label: "Week1 notes",
+    url: "week1/index.html"
+  }
+];
+
+
+function indexLoad(){
+  const ol = document.querySelector("#linksList");
+
+  links.forEach(Link => {
+    const link = document.createElement('li');
+    const href = document.createElement("a");
+    href.setAttribute('href',Link.url);
+    href.innerText = Link.label;
+
+   link.appendChild(href);
+   ol.appendChild(link);
+  })
+}
